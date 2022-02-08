bdbfa2193fbd3dd6f5a64715181c1d35b0377e68
Anderson <andersontno@gmail.com>
2022-02-08 17:35:52 -0300

    Configuração do PostegreSQL

--- setup/settings.py
+++ setup/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buscar_animal',
+        'USER': 'postgres',
+        'PASSWORD': 'teste',
+        'HOST': 'localhost'
     }
 }
 
